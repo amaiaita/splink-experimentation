f8f6765255e2f102ffa2b5b74cb2e880fc3ae568
amaiaita <114224821+amaiaita@users.noreply.github.com>
2024-01-08 16:36:10 +0000

    further tests

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import matplotlib 
 import pandas as pd
 import matplotlib.pyplot as plt
+from splink.datasets import splink_datasets
 
 def format_model_m_and_us(linker):
   model  = linker.save_settings_to_json()
@@ -26,3 +27,4 @@ def plot_m_and_u(df,ax,marker=''):
     ax.set_ylabel('m-probability')
     i+=1  
 
+subset_a = splink_datasets.historical_50k.sample(40000)
